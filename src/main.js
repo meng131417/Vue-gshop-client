9bb932a17a0c6b23de0277154959b63acc46c06a
meng <1310415421@qq.com>
2018-11-17 13:06:16 +0800

    init app

---
+++ src/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by meng on 2018/11/17.
+ */
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+})
